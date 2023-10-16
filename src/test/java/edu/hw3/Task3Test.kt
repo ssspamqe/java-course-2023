8aba73a1ae45db656b0802f73c3bb8347ed526b6
ssspamqe <dylagx@gmail.com>
2023-10-17 02:02:28 +0300

    add Task3

---
+++ src/test/java/edu/hw3/Task3Test.kt
@@ -0,0 +1,29 @@
+package edu.hw3
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class Task3Test {
+    val task = Task3()
+
+    @Test
+    @DisplayName("Method should find frequency of strings in list")
+    fun check_strings(){
+        val list = listOf("код", "код", "код", "bug")
+
+        val frequencyDict  = task.freqDict(list)
+
+        assertThat(frequencyDict).isEqualTo(mapOf("код" to 3, "bug" to 1))
+    }
+
+    @Test
+    @DisplayName("Method should find frequency of integers in list")
+    fun check_int(){
+        val list = listOf(1, 1, 2, 2)
+
+        val frequencyDict  = task.freqDict(list)
+
+        assertThat(frequencyDict).isEqualTo(mapOf(1 to 2, 2 to 2))
+    }
+}
