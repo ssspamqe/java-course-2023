2b6ae1d645f555989f456a8d9f4826389068e98a
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-10-24 11:15:34 +0300

    add Task2
    i just messed up the merge and occasionally deleted task2 files, so im returned them

---
+++ src/main/java/edu/hw2/Task3/FaultyConnection.java
@@ -0,0 +1,20 @@
+package edu.hw2.Task3;
+
+import edu.hw2.Task3.interfaces.Connection;
+import edu.hw2.Task3.interfaces.ConnectionException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FaultyConnection implements Connection {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public void execute(String command) throws ConnectionException {
+        LOGGER.info("Executed command: " + command + " via FaultyConnection");
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
