20dd9fdec4f198245ac0da9a4d8ec519b8a0b591
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-10-30 23:24:51 +0400

    add task2

---
+++ src/main/java/edu/hw5/Task2.kt
@@ -0,0 +1,37 @@
+package edu.hw5
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
+
+class Task2 {
+
+    public fun getAllFriday13th(year:Int) : List<LocalDate>{
+        var date:LocalDate? = LocalDate.of(year,1,1);
+
+        val fridays = mutableListOf<LocalDate>()
+
+        while(date!=null){
+            date = getNextFriday13th(date)
+            if(date != null)
+                fridays.add(date)
+        }
+
+        return fridays
+
+    }
+
+    public fun getNextFriday13th(date:LocalDate?): LocalDate? {
+
+        var currentDate = date
+
+        while(currentDate!!.year==date!!.year){
+            currentDate = currentDate.with(TemporalAdjusters.firstDayOfNextMonth()).plusDays(12)
+
+            if(currentDate.dayOfWeek == DayOfWeek.FRIDAY)
+                return currentDate
+        }
+        return null
+    }
+
+}
