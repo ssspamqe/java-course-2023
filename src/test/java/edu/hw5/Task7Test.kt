ead00783dd180d74f20fe00e7a9b9d3708fb8d28
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-10-31 17:46:49 +0400

    refactor
    replace "returned" by "result" in tests for task 7 and 8

--- src/test/java/edu/hw5/Task7Test.kt
+++ src/test/java/edu/hw5/Task7Test.kt
@@ -11,25 +11,25 @@ class Task7Test:ShouldSpec({
         should("return true if third symbol is zero"){
             val string = "01011"
 
-            val returned = task.thirdSymbolIsZero(string)
+            val result = task.thirdSymbolIsZero(string)
 
-            returned shouldBe true
+            result shouldBe true
         }
 
         should("return false if third symbol is not zero"){
             val string = "001000"
 
-            val returned = task.thirdSymbolIsZero(string)
+            val result = task.thirdSymbolIsZero(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
 
         should("return false if string have length less than 3"){
             val string ="00"
 
-            val returned = task.thirdSymbolIsZero(string)
+            val result = task.thirdSymbolIsZero(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
     }
 
@@ -37,17 +37,17 @@ class Task7Test:ShouldSpec({
         should("return true if string have the same start and end symbols"){
             val string = "1010101"
 
-            val returned = task.haveSameStartAndEnd(string)
+            val result = task.haveSameStartAndEnd(string)
 
-            returned shouldBe true
+            result shouldBe true
         }
 
         should("return false if string have different start and end symbols"){
             val string = "101010110"
 
-            val returned = task.haveSameStartAndEnd(string)
+            val result = task.haveSameStartAndEnd(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
     }
 
@@ -55,17 +55,17 @@ class Task7Test:ShouldSpec({
         should("return true if string.length is in range [1;3]"){
             val string = "111"
 
-            val returned = task.haveLengthInRange13(string)
+            val result = task.haveLengthInRange13(string)
 
-            returned shouldBe true
+            result shouldBe true
         }
 
         should("return false if string.length is not in range[1;3]"){
             val string = "10001"
 
-            val returned = task.haveLengthInRange13(string)
+            val result = task.haveLengthInRange13(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
     }
 
--- src/test/java/edu/hw5/Task8Test.kt
+++ src/test/java/edu/hw5/Task8Test.kt
@@ -11,17 +11,17 @@ class Task8Test: ShouldSpec({
         should("return true if string have odd length"){
             val string = "011"
 
-            val returned = task.check0(string)
+            val result = task.check0(string)
 
-            returned shouldBe true
+            result shouldBe true
         }
 
         should("return false if string have even length"){
             val string = "0111"
 
-            val returned = task.check0(string)
+            val result = task.check0(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
     }
 
@@ -29,33 +29,33 @@ class Task8Test: ShouldSpec({
         should("return true if string starts with 0 and have odd length"){
             val string = "011"
 
-            val returned = task.check1(string)
+            val result = task.check1(string)
 
-            returned shouldBe true
+            result shouldBe true
         }
 
         should("return true if string starts with 1 and have even length"){
             val string = "1101"
 
-            val returned = task.check1(string)
+            val result = task.check1(string)
 
-            returned shouldBe true
+            result shouldBe true
         }
 
         should("return false if string starts with 0 and have even length"){
             val string = "0000"
 
-            val returned = task.check1(string)
+            val result = task.check1(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
 
         should("return false if string starts with 1 and have odd length"){
             val string = "100"
 
-            val returned = task.check1(string)
+            val result = task.check1(string)
 
-            returned shouldBe false
+            result shouldBe false
         }
     }
 
