7eda20b71aa5c136ff18ee7827c64dedf622bedd
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-03 01:27:50 +0300

    add the most popular get resourse analyser

--- src/main/java/edu/project3/LogAnalyser.kt
+++ src/main/java/edu/project3/LogAnalyser.kt
@@ -6,4 +6,17 @@ class LogAnalyser {
         logs.size
 
 
+    fun getTheMostPopularGETResources(logs:List<Map<String,String>>):List<String>{
+
+        val occurrences = logs
+            .filter { it["request_type"] == "GET" }
+            .map { it["request"] }
+            .groupingBy { it }
+            .eachCount()
+
+        val max = occurrences.map { it.value }.max()
+
+        return occurrences.filter { it.value == max }.map { it.key!! }.toList()
+    }
+
 }
