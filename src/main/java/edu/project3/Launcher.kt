e08cd7ea06b91471ad217c57496a61c6b3c5cfeb
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-04 01:47:19 +0300

    add overall data printting

--- src/main/java/edu/project3/Launcher.kt
+++ src/main/java/edu/project3/Launcher.kt
@@ -4,6 +4,7 @@ import edu.project3.logWorkers.LogAnalyser
 import edu.project3.logWorkers.LogParser
 import edu.project3.tablePrinters.ADocTablePrinter
 import edu.project3.tablePrinters.MarkdownTablePrinter
+import org.apache.logging.log4j.LogManager
 import java.io.File
 import java.net.URL
 import java.time.LocalDate
@@ -17,13 +18,21 @@ var format: String? = null
 val logAnalyser = LogAnalyser()
 val logParser = LogParser()
 
+val LOGGER = LogManager.getLogger()
+
 fun main(params: Array<String>) {
     parseParams(params)
 
     var logs = logParser.parseAllLogs(getNonParsedSources())
     logs = logAnalyser.getDateConstrainedLogs(logs,from,to)
 
-    ADocTablePrinter().printListOfMaps(logs)
+    MarkdownTablePrinter().printListOfMaps(logs)
+
+    LOGGER.info("\n")
+
+    printOverallData(logs)
+
+
 }
 
 private fun parseParams(params: Array<String>) {
@@ -78,5 +87,24 @@ private fun getNonParsedSources():List<String>{
     }
 
     return nonParsedSources.toList()
+}
+
+private fun printOverallData(logs:List<Map<String,String>>){
+
+    val table = mutableListOf<Map<String,String>>()
+
+    table.addAll(listOf(
+        mapOf("metrics" to "Files", "value" to sources.toString()),
+        mapOf("metrics" to "Start date", "value" to from.toString()),
+        mapOf("metrics" to "End date", "value" to to.toString()),
+        mapOf("metrics" to "Requests","value" to logAnalyser.getRequestsAmount(logs).toString()),
+        mapOf("metrics" to "Average response size", "value" to logAnalyser.getAverageResponseSize(logs).toString())
+    ))
+
+    printTable(table)
+}
+
+private fun printTable(table:List<Map<String,String>>){
+    MarkdownTablePrinter().printListOfMaps(table)
 }
 
