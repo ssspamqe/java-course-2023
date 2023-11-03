e13f4ae3f97764950e8d55efd8c234e4704dbec5
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-03 13:24:12 +0300

    refacdtor
    remove explicit OR operators in regexs

--- src/main/java/edu/hw5/Task7.kt
+++ src/main/java/edu/hw5/Task7.kt
@@ -3,12 +3,12 @@ package edu.hw5
 class Task7 {
 
     public fun thirdSymbolIsZero(line: String): Boolean =
-        line.matches("[0|1]{2}0[0|1]*".toRegex())
+        line.matches("[01]{2}0[0|1]*".toRegex())
 
     public fun haveSameStartAndEnd(line: String): Boolean =
         line.matches("^(0|1).*\\1".toRegex())
 
     public fun haveLengthInRange13(line: String): Boolean =
-        line.matches("[0|1]{1,3}".toRegex())
+        line.matches("[01]{1,3}".toRegex())
 
 }
