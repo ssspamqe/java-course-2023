138edc537de8de8d1c544a03a778261479113032
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-09 23:19:41 +0300

    fix parser in task5

---
+++ src/test/java/edu/hw6/Task5/Task5Test.java
@@ -0,0 +1,25 @@
+package edu.hw6.Task5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task5Test {
+
+    HackerNews hackerNews = new HackerNews();
+
+    @Test
+    @DisplayName("getNewsName() should return a name of news by given id")
+    void getNewsName_should_returnNewsName_byGivenId() throws Exception {
+        var correctNewsName = "JDK 21 Release Notes";
+        var newsId = 37570037;
+
+        var returnedNewsName = hackerNews.getNewsName(newsId);
+
+        //System.out.println(returnedNewsName);
+        assertThat(returnedNewsName).isEqualTo(correctNewsName);
+    }
+
+}
