c3b0fb94bee86375ccf879aab2e4f0d8ba7a2324
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-11 00:25:18 +0300

    add tests for task1

--- src/test/java/edu/hw6/Task1/Task1Test.java
+++ src/test/java/edu/hw6/Task1/Task1Test.java
@@ -3,11 +3,14 @@ package edu.hw6.Task1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -127,6 +130,84 @@ public class Task1Test {
 
 
         assertThat(returnedEntries).containsExactlyInAnyOrderElementsOf(actualEntries);
+    }
+
+    @Test
+    @DisplayName("values() should return a collection of values of diskMap")
+    void values_should_return_valuesOfDiskMap(){
+        var firstKey = "fKey";
+        var firstValue = "fVal";
+        var secondKey = "sKey";
+        var secondValue = "sVal";
+
+        var actualValues = List.of(firstValue,secondValue);
+
+
+        diskMap.put(firstKey,firstValue);
+        diskMap.put(secondKey,secondValue);
+        var returnedValues = diskMap.values();
+
+
+        assertThat(returnedValues).containsExactlyInAnyOrderElementsOf(actualValues);
+    }
+
+
+    @Test
+    @DisplayName("keySet() should return a set of keys")
+    void keySet_should_return_aSetOfKeys(){
+        var firstKey = "fKey";
+        var firstValue = "fVal";
+        var secondKey = "sKey";
+        var secondValue = "sVal";
+
+        var actualKeys = Set.of(firstKey,secondKey);
+
+
+        diskMap.put(firstKey,firstValue);
+        diskMap.put(secondKey,secondValue);
+        var returnedKeys = diskMap.keySet();
+
+
+        assertThat(returnedKeys).containsExactlyInAnyOrderElementsOf(actualKeys);
+    }
+
+    @Test
+    @DisplayName("clear() should clear the diskMap")
+    void clear_should_clearTheDiskMap(){
+        var key = "key";
+        var value = "val";
+
+
+        diskMap.put(key,value);
+        diskMap.clear();
+        var returnedVal = diskMap.get(key);
+
+
+        assertThat(returnedVal).isNull();
+    }
+
+    @Test
+    @DisplayName("Disk map should be able to count it's size")
+    void diskMap_should_countSize(){
+        assertThat(diskMap.size()).isEqualTo(0);
+
+        diskMap.put("key","val");
+        assertThat(diskMap.size()).isEqualTo(1);
+
+        diskMap.put("key2","val2");
+        assertThat(diskMap.size()).isEqualTo(2);
+
+        diskMap.remove("adadsa");
+        assertThat(diskMap.size()).isEqualTo(2);
+
+        diskMap.remove("key","wrong value");
+        assertThat(diskMap.size()).isEqualTo(2);
+
+        diskMap.remove("key");
+        assertThat(diskMap.size()).isEqualTo(1);
+
+        diskMap.clear();
+        assertThat(diskMap.isEmpty()).isTrue();
     }
 
 
