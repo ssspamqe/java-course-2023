0a5122244a222bcea3579bc6244c53a3714d9ef9
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-15 12:23:01 +0300

    use Parameterized tests

--- src/test/java/edu/hw6/Task6/PortScannerTest.java
+++ src/test/java/edu/hw6/Task6/PortScannerTest.java
@@ -1,49 +1,39 @@
 package edu.hw6.Task6;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PortScannerTest {
 
     String portLinePattern = PortScanner.PORT_LINE_PATTERN;
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "1001 - my service",
+        "2513 - service1",
+        "0 - unknown serviiiice",
+        "12 - -asda"
+    })
     @DisplayName("portLinePatter should match to lines of type \"[port] - [service]\"")
-    void portLinePattern_should_matchToCorrectLines() {
-        List<String> lines = List.of(
-            "1001 - my service",
-            "2513 - service1",
-            "0 - unknown serviiiice",
-            "12 - -asda"
-        );
+    void portLinePattern_should_matchToCorrectLines(String line) {
+        boolean matches = line.matches(portLinePattern);
 
-        List<Boolean> matches =
-            lines.stream()
-                .map(it -> it.matches(portLinePattern))
-                .toList();
-
-        assertThat(matches).doesNotContain(false);
+        assertThat(matches).isTrue();
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "1001 -my service",
+        "2513 -  service1",
+        "-1 - unknown serviiiice"
+    })
     @DisplayName("portLinePatter should not match to lines of not \"[port] - [service]\" type")
-    void portLinePattern_should_not_matchToIncorrectLines() {
-        List<String> lines = List.of(
-            "1001 -my service",
-            "2513 -  service1",
-            "-1 - unknown serviiiice"
-        );
+    void portLinePattern_should_not_matchToIncorrectLines(String line) {
+        boolean matches = line.matches(portLinePattern);
 
-        List<Boolean> matches =
-            lines.stream()
-                .map(it -> it.matches(portLinePattern))
-                .toList();
-
-        assertThat(matches).doesNotContain(true);
+        assertThat(matches).isFalse();
     }
 
 }
