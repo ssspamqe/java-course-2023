322f4148168608e14a952b0843d824681255c32f
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-16 13:26:16 +0300

    reformat task2

--- src/main/java/edu/hw7/Task2.java
+++ src/main/java/edu/hw7/Task2.java
@@ -1,11 +1,16 @@
 package edu.hw7;
 
+import jakarta.validation.constraints.Positive;
 import java.util.stream.IntStream;
 
 public class Task2 {
 
-    public static int getFactorial(int num) {
-        return IntStream.rangeClosed(1, num).parallel().reduce((a, b) -> a * b).getAsInt();
+    public static int getFactorial(@Positive int num) {
+        return IntStream
+            .rangeClosed(1, num)
+            .parallel()
+            .reduce((a, b) -> a * b)
+            .getAsInt();
     }
 
 }
