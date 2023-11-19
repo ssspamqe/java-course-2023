e367a06f523bb2761c118698a6f973454b959486
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-19 18:51:07 +0300

    [hw7 - task3] rework AbstractPersonDB

--- src/main/java/edu/hw7/Task3/AbstractPersonDB.java
+++ src/main/java/edu/hw7/Task3/AbstractPersonDB.java
@@ -1,133 +1,101 @@
 package edu.hw7.Task3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class AbstractPersonDB implements PersonDB {
-    List<Person> persons = new ArrayList<>();
+
+    private int maxId = -1;
+
+    Map<Integer, Person> personsById = new HashMap<>();
+    Map<String, List<Person>> personsByName = new HashMap<>();
+    Map<String, List<Person>> personsByAddress = new HashMap<>();
+    Map<String, Person> personsByPhone = new HashMap<>();
 
     @Override
     public void add(Person newPerson) {
-        updateDB(newPerson);
+        if(!phoneIsAvailable(newPerson.phoneNumber()))
+            return;
+
+        Person personWithId = getPersonWithCorrectId(newPerson);
+        addToDB(newPerson);
     }
 
     @Override
     public void delete(int id) {
-        persons.remove(id);
+        Person person = personsById.get(id);
+        deletePersonInPersonsByName(person);
+        deletePersonInPersonsByAddress(person);
+        deletePersonInPersonsByPhone(person);
     }
 
     @Override
-    public @Nullable Person findByName(String name) {
-        return persons
-            .stream()
-            .filter(person ->
-                person
-                    .name()
-                    .equals(name)
-
-                    && person
-                    .dontHaveNullFields()
-            )
-            .findFirst()
-            .orElse(null);
+    public @Nullable List<Person> findByName(String name) {
+        return personsByName.get(name);
     }
 
     @Override
-    public @Nullable Person findByAddress(String address) {
-        return persons
-            .stream()
-            .filter(person ->
-                person
-                    .address()
-                    .equals(address)
-
-                    && person
-                    .dontHaveNullFields()
-            )
-            .findFirst()
-            .orElse(null);
+    public @Nullable List<Person> findByAddress(String address) {
+        return personsByAddress.get(address);
     }
 
     @Override
     public @Nullable Person findByPhone(String phone) {
-        return persons
-            .stream()
-            .filter(person ->
-                person
-                    .address()
-                    .equals(phone)
-
-                    && person
-                    .dontHaveNullFields()
-            )
-            .findFirst()
-            .orElse(null);
+        return personsByPhone.get(phone);
     }
 
-    private Person getMergedPerson(Person oldPerson, Person newPerson) {
-        if (oldPerson.id() == null) {
-            return null;
+    protected void addToDB(Person person){
+        addPersonToPersonsByName(person);
+        addPersonToPersonsByAddress(person);
+        addPersonToPersonByPhone(person);
+    }
+
+    private void deletePersonInPersonsByName(Person person) {
+        personsByName.values().forEach(it -> it.remove(person));
+    }
+
+    private void deletePersonInPersonsByAddress(Person person) {
+        personsByAddress.values().forEach(it -> it.remove(person));
+    }
+
+    private void deletePersonInPersonsByPhone(Person person) {
+        String phone = person.phoneNumber();
+        personsByPhone.remove(phone);
+    }
+
+    private void addPersonToPersonsByName(Person person){
+        String name = person.name();
+        personsByName.computeIfAbsent(name, _ -> new ArrayList<>());
+        personsByName.get(name).add(person);
+    }
+
+    private void addPersonToPersonsByAddress(Person person){
+        String address = person.address();
+        personsByAddress.computeIfAbsent(address,_ -> new ArrayList<>());
+        personsByAddress.get(address).add(person);
+    }
+
+    private void addPersonToPersonByPhone(Person person){
+        String phone = person.phoneNumber();
+        if (phoneIsAvailable(phone)) {
+            personsByPhone.put(phone,person);
         }
-
-        var id = oldPerson.id();
-
-        String name = getMergedName(oldPerson, newPerson);
-        String address = getMergedAddress(oldPerson, newPerson);
-        String phone = getMergedPhone(oldPerson, newPerson);
-
-        return new Person(id, name, address, phone);
     }
 
-    private String getMergedName(Person oldPerson, Person newPerson) {
-        if (newPerson.name() != null) {
-            return newPerson.name();
+    private Person getPersonWithCorrectId(Person person) {
+        if (person.id() == null) {
+            maxId++;
+            return person.gtPersonWithId(maxId);
         }
-        return oldPerson.name();
+        maxId = Math.max(person.id(), maxId);
+        return person;
     }
 
-    private String getMergedAddress(Person oldPerson, Person newPerson) {
-        if (newPerson.address() != null) {
-            return newPerson.address();
-        }
-        return oldPerson.address();
-    }
-
-    private String getMergedPhone(Person oldPerson, Person newPerson) {
-        if (newPerson.phoneNumber() != null) {
-            return newPerson.phoneNumber();
-        }
-        return oldPerson.phoneNumber();
-    }
-
-    protected void mergePerson(Person oldPerson, Person newPerson) {
-        Person mergedPerson = getMergedPerson(oldPerson, newPerson);
-        int id = mergedPerson.id();
-        persons.set(id, mergedPerson);
-    }
-
-    protected void updateDB(Person newPerson) {
-        var personByName = findByName(newPerson.name());
-        var personByAddress = findByAddress(newPerson.address());
-        var personByPhone = findByPhone(newPerson.phoneNumber());
-
-        if (personByName != null) {
-            mergePerson(personByName, newPerson);
-        } else if (personByAddress != null) {
-            mergePerson(personByAddress, newPerson);
-        } else if (personByPhone != null) {
-            mergePerson(personByPhone, newPerson);
-        } else {
-            Person personWithId =
-                new Person(
-                    persons.size(),
-                    newPerson.name(),
-                    newPerson.address(),
-                    newPerson.phoneNumber()
-                );
-            persons.add(personWithId);
-        }
+    private boolean phoneIsAvailable(String phone) {
+        return personsByPhone.get(phone) == null;
     }
 
 }
