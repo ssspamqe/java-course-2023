34c2096f376460ee8fd8793e90fdd0482a3e6555
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-20 22:13:50 +0300

    [prj 4] add PointFunction interface

---
+++ src/main/java/edu/project4/fractalGeneration/pointModifiers/pointFunctions/PointFunction.java
@@ -0,0 +1,10 @@
+package edu.project4.fractalGeneration.pointModifiers.pointFunctions;
+
+import edu.project4.fractalGeneration.point.DoublePoint;
+
+public interface PointFunction {
+
+    public DoublePoint get(DoublePoint oldPoint);
+
+
+}
