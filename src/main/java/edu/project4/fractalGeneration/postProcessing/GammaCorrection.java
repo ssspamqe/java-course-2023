392591dd26ca8be0766bfcfc5cdda1ab2fbf6be7
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-21 11:37:24 +0300

    [prj 4] fix gamma correction

--- src/main/java/edu/project4/fractalGeneration/postProcessing/GammaCorrection.java
+++ src/main/java/edu/project4/fractalGeneration/postProcessing/GammaCorrection.java
@@ -3,15 +3,20 @@ package edu.project4.fractalGeneration.postProcessing;
 import edu.project4.fractalGeneration.graphics.Pixel;
 import edu.project4.fractalGeneration.graphics.PixelCanvas;
 import java.awt.Color;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GammaCorrection implements PostProcessing {
+
+    private static final double GAMMA =3;
     @Override
     public void applyProcedure(PixelCanvas canvas) {
         int height = canvas.getHeight();
         int width = canvas.getWidth();
 
+        Set<Pixel> correctedPixels = new HashSet<>();
+
         double maxGamma = 0;
-        double gamma = 2.2;
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
@@ -24,18 +29,21 @@ public class GammaCorrection implements PostProcessing {
         }
 
         for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                var pixel = canvas.getPixel(row, col);
-                rePaintPixel(pixel, gamma, maxGamma);
+            for (int column = 0; column < width; column++) {
+                var pixel = canvas.getPixel(row, column);
+                if(!correctedPixels.contains(pixel)) {
+                    rePaintPixel(pixel, maxGamma);
+                }
+                correctedPixels.add(pixel);
             }
         }
 
     }
 
-    private static void rePaintPixel(Pixel pixel, double gamma, double maxGamma) {
+    private static void rePaintPixel(Pixel pixel, double maxGamma) {
         pixel.setNormal(pixel.getNormal() / maxGamma);
 
-        double coefficient = Math.pow(pixel.getNormal(), (1 / gamma));
+        double coefficient = Math.pow(pixel.getNormal(), (1 / GAMMA));
 
         int red = (int) (pixel.getColor().getRed() * coefficient);
         int green = (int) (pixel.getColor().getGreen() * coefficient);
