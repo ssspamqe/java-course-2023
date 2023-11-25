97990fb94b79983e246a1353297a7bcee6b946e2
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-11-25 14:16:01 +0300

    [prj 4] refactor gammaCorrection

--- src/main/java/edu/project4/fractalGeneration/postProcessing/GammaCorrection.java
+++ src/main/java/edu/project4/fractalGeneration/postProcessing/GammaCorrection.java
@@ -12,25 +12,15 @@ public class GammaCorrection implements PostProcessing {
 
     @Override
     public void applyProcedure(PixelCanvas canvas) {
-        int height = canvas.getHeight();
-        int width = canvas.getWidth();
+        setPixelNormal(canvas);
+        double maxGamma = getMaxGamma(canvas);
+        rePaintCanvas(canvas,maxGamma);
+    }
 
+    private void rePaintCanvas(PixelCanvas canvas, double maxGamma){
         Set<Pixel> correctedPixels = new HashSet<>();
-
-        double maxGamma = 0;
-
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                var pixel = canvas.getPixel(row, col);
-                if (pixel.getHits() != 0) {
-                    pixel.setNormal(Math.log10(pixel.getHits()));
-                    maxGamma = Math.max(maxGamma, pixel.getNormal());
-                }
-            }
-        }
-
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < width; column++) {
+        for (int row = 0; row < canvas.getHeight(); row++) {
+            for (int column = 0; column < canvas.getWidth(); column++) {
                 var pixel = canvas.getPixel(row, column);
                 if (!correctedPixels.contains(pixel)) {
                     rePaintPixel(pixel, maxGamma);
@@ -40,7 +30,7 @@ public class GammaCorrection implements PostProcessing {
         }
     }
 
-    private static void rePaintPixel(Pixel pixel, double maxGamma) {
+    private void rePaintPixel(Pixel pixel, double maxGamma) {
         pixel.setNormal(pixel.getNormal() / maxGamma);
 
         double coefficient = Math.pow(pixel.getNormal(), (1 / GAMMA));
@@ -52,6 +42,33 @@ public class GammaCorrection implements PostProcessing {
         Color newColor = new Color(red, green, blue);
 
         pixel.setColor(newColor);
+    }
+
+
+    private void setPixelNormal(PixelCanvas canvas){
+        for (int row = 0; row < canvas.getHeight(); row++) {
+            for (int col = 0; col < canvas.getWidth(); col++) {
+                var pixel = canvas.getPixel(row, col);
+                if (pixel.getHits() != 0) {
+                    pixel.setNormal(Math.log10(pixel.getHits()));
+                }
+            }
+        }
+    }
+
+    private double getMaxGamma(PixelCanvas canvas) {
+        double maxGamma = Double.MIN_VALUE;
+
+        for (int row = 0; row < canvas.getHeight(); row++) {
+            for (int col = 0; col < canvas.getWidth(); col++) {
+                var pixel = canvas.getPixel(row, col);
+                if (pixel.getHits() != 0) {
+                    maxGamma = Math.max(maxGamma, pixel.getNormal());
+                }
+            }
+        }
+
+        return maxGamma;
     }
 
 }
