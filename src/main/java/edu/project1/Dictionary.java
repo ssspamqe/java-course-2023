18be1f53c3445b3cbaa99520134203506411229b
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-12-01 21:34:14 +0300

    [prj1] refactor Dictionarry

--- src/main/java/edu/project1/Dictionary.java
+++ src/main/java/edu/project1/Dictionary.java
@@ -1,11 +1,25 @@
 package edu.project1;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 class Dictionary {
-    private List<String> dictionary = List.of("machine", "house", "theatre");
+
+    private static final String DEFAULT_WORDS_PATH = "./src/main/java/edu/project1/words.txt";
+    private static final String ONLY_ENGLISH_LETTERS_PATTERN = "[a-zA-z]+";
+
+    private List<String> dictionary = List.of();
+
+    public Dictionary(String filePath) {
+        loadFile(filePath);
+    }
+
+    public Dictionary() {
+        this(DEFAULT_WORDS_PATH);
+    }
 
     public String getRandomWord() {
         return dictionary.get(ThreadLocalRandom.current().nextInt(dictionary.size()));
@@ -16,18 +30,25 @@ class Dictionary {
     }
 
     public void addNewWord(String newWord) {
-        String lowerCaseWord = newWord.toLowerCase();
-        for (int i = 0; i < lowerCaseWord.length(); i++) {
-            if (!Character.isLetter(lowerCaseWord.charAt(i))) {
-                throw new IllegalArgumentException("Word must contain only lower english letters");
-            }
+        if (!newWord.matches(ONLY_ENGLISH_LETTERS_PATTERN)) {
+            throw new RuntimeException("Word must contain only english letters");
         }
-        dictionary = addElement(dictionary, lowerCaseWord);
+        dictionary = addElement(dictionary, newWord.toLowerCase());
     }
 
     private <T> List<T> addElement(List<T> list, T newElement) {
         var bufArrayList = new ArrayList<>(list);
         bufArrayList.add(newElement);
         return bufArrayList.stream().toList();
+    }
+
+    private void loadFile(String filePath) {
+        try (Scanner scanner = new Scanner(new FileInputStream(filePath))) {
+            while (scanner.hasNextLine()) {
+                addNewWord(scanner.nextLine());
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
