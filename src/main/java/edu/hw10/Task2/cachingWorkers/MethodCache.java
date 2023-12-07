b69226c5bada0ad0dc40b64bf59a627678c46b2e
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-12-07 14:28:33 +0300

    [hw10 - task2] simplify cache classes
    delete any locks and soft references

--- src/main/java/edu/hw10/Task2/cachingWorkers/MethodCache.java
+++ src/main/java/edu/hw10/Task2/cachingWorkers/MethodCache.java
@@ -6,59 +6,21 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-/**
- * This class is thread safe, uses SoftReferences
- */
 public class MethodCache {
 
-    private static final ReadWriteLock LOCK = new ReentrantReadWriteLock(true);
-
     private final Method method;
-    private SoftReference<Map<List<Object>, Object>> cacheRef;
+    private final Map<List<Object>, Object> cache = new HashMap<>();
 
     public MethodCache(Method method) {
         this.method = method;
-        cacheRef = new SoftReference<>(new HashMap<>());
     }
 
     public Object readResult(Object[] args) {
-        var cache = getCurrentCache();
-
-        LOCK.readLock().lock();
-        try {
-            if (cache == null) {
-                return null;
-            }
-            return cache.get(Arrays.asList(args));
-        } finally {
-            LOCK.readLock().unlock();
-        }
+        return cache.get(Arrays.asList(args));
     }
 
     public void writeResult(Object[] args, Object result) {
-        var cache = getCurrentCache();
-
-        LOCK.writeLock().lock();
-        try {
-            if (cache == null) {
-                cacheRef =new SoftReference<>(new HashMap<>());
-                cache = cacheRef.get();
-            }
-            cache.put(Arrays.asList(args), result);
-        } finally {
-            LOCK.writeLock().unlock();
-        }
-    }
-
-    private Map<List<Object>, Object> getCurrentCache() {
-        LOCK.readLock().lock();
-        try {
-            return cacheRef.get();
-        } finally {
-            LOCK.readLock().unlock();
-        }
+        cache.put(Arrays.asList(args), result);
     }
 }
--- src/main/java/edu/hw10/Task2/cachingWorkers/ObjectCache.java
+++ src/main/java/edu/hw10/Task2/cachingWorkers/ObjectCache.java
@@ -7,58 +7,23 @@ import java.util.Map;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-//This class is thread safe
 public class ObjectCache {
 
-    private static final ReadWriteLock LOCK = new ReentrantReadWriteLock(true);
-
     private final Object object;
-    private SoftReference<Map<Method, MethodCache>> methodCachesRef;
+    private final Map<Method, MethodCache> methodCaches = new HashMap<>();
 
     public ObjectCache(Object object) {
         this.object = object;
-        methodCachesRef = new SoftReference<>(new HashMap<>());
     }
 
     public Object readResult(Method method, Object[] args) {
-        var methodCaches = getCurrentMethodCaches();
-
-        LOCK.readLock().lock();
-        try {
-            if (methodCaches == null) {
-                return null;
-            }
-            return methodCaches.get(method).readResult(args);
-        } finally {
-            LOCK.readLock().unlock();
-        }
+        return methodCaches.get(method).readResult(args);
     }
 
     public void writeResult(Method method, Object[] args, Object result) {
-        var methodCaches = getCurrentMethodCaches();
-
-        LOCK.writeLock().lock();
-        try {
-            if (methodCaches == null) {
-                methodCachesRef = new SoftReference<>(new HashMap<>());
-                methodCaches = methodCachesRef.get();
-            }
-
-            if (!methodCaches.containsKey(method)) {
-                methodCaches.put(method, new MethodCache(method));
-            }
-            methodCaches.get(method).writeResult(args, result);
-        } finally {
-            LOCK.writeLock().unlock();
+        if (!methodCaches.containsKey(method)) {
+            methodCaches.put(method, new MethodCache(method));
         }
-    }
-
-    private Map<Method, MethodCache> getCurrentMethodCaches() {
-        LOCK.readLock().lock();
-        try {
-            return methodCachesRef.get();
-        } finally {
-            LOCK.readLock().unlock();
-        }
+        methodCaches.get(method).writeResult(args, result);
     }
 }
