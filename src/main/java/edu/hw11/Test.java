f46c028256b5e5c2f67ce5f19c1300466e76cd61
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-12-15 01:09:31 +0300

    [hw11-task3] add creation of new class in runtime

--- src/main/java/edu/hw11/Test.java
+++ src/main/java/edu/hw11/Test.java
@@ -1,26 +1,26 @@
 package edu.hw11;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
-import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        abstract class SumExample {
-            public abstract long calculate(int a);
-        }
-
         var a = new ByteBuddy()
-            .subclass(SumExample.class)
-            .method(named("calculate"))
+            .subclass(Object.class)
+            .defineMethod("calculate", long.class, Visibility.PUBLIC)
+            .withParameters(int.class)
+            //.method(named("calculate"))
             .intercept(SumImplementation.INSTANCE)
             .make()
             .load(ClassLoader.getSystemClassLoader(), ClassLoadingStrategy.Default.INJECTION)
             .getLoaded();
 
         var instance = a.newInstance();
+        var method = a.getMethod("calculate", int.class);
 
-        System.out.println(instance.calculate(47));
+        System.out.println(method.invoke(instance, 10));
+
     }
 }
