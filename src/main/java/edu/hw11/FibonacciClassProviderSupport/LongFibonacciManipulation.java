dab04b273e6e536a38dfb5f93e4141dfca6a65de
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2023-12-20 17:37:30 +0300

    [hw11 - task3] add comments

--- src/main/java/edu/hw11/FibonacciClassProviderSupport/LongFibonacciManipulation.java
+++ src/main/java/edu/hw11/FibonacciClassProviderSupport/LongFibonacciManipulation.java
@@ -26,23 +26,23 @@ class LongFibonacciManipulation implements StackManipulation {
         Implementation.Context implementationContext
     ) {
         mv.visitCode();
-
-        mv.visitVarInsn(Opcodes.ILOAD, 0);
-        mv.visitInsn(Opcodes.ICONST_1);
+        mv.visitVarInsn(Opcodes.ILOAD, 0); //loading method argument to stack
+        mv.visitInsn(Opcodes.ICONST_1); //loading 1 to stack
 
         Label elseLabel = new Label();
-        mv.visitJumpInsn(Opcodes.IF_ICMPGT, elseLabel);
+        mv.visitJumpInsn(Opcodes.IF_ICMPGT, elseLabel); //if argument is greater than 1, than jump to "elseLabel"
         mv.visitVarInsn(Opcodes.ILOAD, 0);
-        mv.visitInsn(Opcodes.I2L);
+        mv.visitInsn(Opcodes.I2L); //loading argument to the stack and casting it to long
         mv.visitInsn(Opcodes.LRETURN);
-        mv.visitLabel(elseLabel);
 
+        mv.visitLabel(elseLabel);
+        //checking frame state after jump instruction (ByteBuddy's feature)
         mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
 
         mv.visitVarInsn(Opcodes.ILOAD, 0);
         mv.visitInsn(Opcodes.ICONST_1);
-        mv.visitInsn(Opcodes.ISUB);
-        mv.visitMethodInsn(
+        mv.visitInsn(Opcodes.ISUB); //loading arg-1 to the top of the stack
+        mv.visitMethodInsn ( //calling method recursively, passing arg-1
             Opcodes.INVOKESTATIC,
             METHOD_OWNER,
             FibonacciClassProvider.METHOD_NAME,
@@ -52,8 +52,8 @@ class LongFibonacciManipulation implements StackManipulation {
 
         mv.visitVarInsn(Opcodes.ILOAD, 0);
         mv.visitInsn(Opcodes.ICONST_2);
-        mv.visitInsn(Opcodes.ISUB);
-        mv.visitMethodInsn(
+        mv.visitInsn(Opcodes.ISUB); //loading arg-2 to the top of the stack
+        mv.visitMethodInsn ( //calling method recursively, passing arg-2
             Opcodes.INVOKESTATIC,
             METHOD_OWNER,
             FibonacciClassProvider.METHOD_NAME,
@@ -61,7 +61,7 @@ class LongFibonacciManipulation implements StackManipulation {
             false
         );
 
-        mv.visitInsn(Opcodes.LADD);
+        mv.visitInsn(Opcodes.LADD); //summing up two returned long values and loading it to the top of the stack
         mv.visitInsn(Opcodes.LRETURN);
 
         mv.visitEnd();
@@ -72,7 +72,6 @@ class LongFibonacciManipulation implements StackManipulation {
     private static String getMethodOwner() {
         return String.join("/", FibonacciClassProvider.CLASS_NAME.split("[.]"));
     }
-
 
     public static StackManipulation getInstance() {
         return SingletonHolder.INSTANCE;
